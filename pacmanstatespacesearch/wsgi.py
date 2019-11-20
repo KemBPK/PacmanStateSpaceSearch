f2bcd00adf06f576b5d798b989e764b365047de0
KemBPK <kembpk@gmail.com>
2019-11-20 00:47:46 -0800

    intentionally adding error

--- pacmanstatespacesearch/wsgi.py
+++ pacmanstatespacesearch/wsgi.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+# from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pacmanstatespacesearch.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pacmanstatespacesearch.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+
+def application(environ, start_response):
+    if environ['mod_wsgi.process_group'] != '': 
+        import signal
+        os.kill(os.getpid(), signal.SIGINT)
+    return ["killed"]
